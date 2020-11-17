43093eb09705593b5d681a22cee8ad655fdf6b5d
Elangel <elangelshaday@gmail.com>
2020-11-17 22:25:03 +0700

    Prepare Quiz PBO

---
+++ src/DatabaseHandler.java
@@ -0,0 +1,63 @@
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.TimeZone;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 1119038 Elangel Neilea Shaday
+ */
+public class DatabaseHandler {
+    public Connection con;
+    private final String driver = "com.mysql.cj.jdbc.Driver";
+    private final String url = "jdbc:mysql://localhost/quis2?serverTimezone=" + TimeZone.getDefault().getID();
+    private final String username = "root";
+    private final String password = "";
+    
+    private Connection logOn() {
+        try {
+            Class.forName(driver).newInstance();
+            con = DriverManager.getConnection(url, username, password);
+        } catch (Exception ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getLocalizedMessage());
+            JOptionPane.showMessageDialog(null, "Error Ocurred when login" + ex);
+        }
+        return con;
+    }
+
+    private void logOff() {
+        try {
+            //tutup koneksi
+            con.close();
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error Ocurred when login" + ex);
+        }
+    }
+
+    public void connect() {
+        try {
+            con = logOn();
+        } catch (Exception ex) {
+            System.out.println("Error occured when connecting to database");
+        }
+    }
+
+    public void disconnect() {
+        try {
+            logOff();
+        } catch (Exception ex) {
+            System.out.println("Error occured when connecting to database");
+        }
+    }
+}
